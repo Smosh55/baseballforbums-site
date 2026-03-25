7f8e7b6347817635887f2b67a9c02aa14a103ade
Smosh55 <moshz17@gmail.com>
2026-03-25 15:03:41 -0400

    Update shared.js

--- shared.js
+++ shared.js
@@ -1,6 +1,4 @@
-const GOOGLE_ENDPOINT = window.GOOGLE_SHEETS_ENDPOINT || "https://script.google.com/macros/echo?user_content_key=AWDtjMXoBOV2fGCynmT4giffMbd6ZB2UchKvjBxVn5T78nQ_UMRn25vpItVX2JHIJvG1m2EkEULWgSqXRvEzRBhLliB4iNk6CI9JY8jbj6qVFnRE3ow5mt1TnaKt4yK_IRcJOic7aTVBkOuIdCaTUl6GOmwswbPGIEel9dSUtPIH_Ymd1WVf9YMTDaExKBxJuUUx2SL4XdO9lrbAF0XazAhOo-_g72rySlO46kALpVHv946BszLSbROGCAxq1o8_xCZeg_EvuTmXNDqbmgFSPfEA0OhFiQh8dA&lib=MKbm9YOGkD3DUziKlwIE4N2CZ_t_9oIdE";
-async function loadData(sheetName, localPath) {
-  if (GOOGLE_ENDPOINT) {
+const GOOGLE_ENDPOINT = window.GOOGLE_SHEETS_ENDPOINT = "https://script.google.com/macros/s/AKfycbxFbBzXrtaB9GTRHWrfUTzHtEkUDZoXCd5-hqSZ0UUOMyHmoEFgo5cEWi87duOUt-LO/exec";
     const url = `${GOOGLE_ENDPOINT}?sheet=${encodeURIComponent(sheetName)}`;
     const res = await fetch(url);
     if (!res.ok) throw new Error(`Failed to fetch ${sheetName}`);
